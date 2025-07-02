64c715610b3756fea2143d027e32a24d56ecb22c
winminko-star <winminthuzar1@gmail.com>
2025-07-02 15:57:27 +0800

    Update UserPanel.jsx

--- src/pages/UserPanel.jsx
+++ src/pages/UserPanel.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import { products } from "../data/products";
+import products from "../data/products";
 
 const TABLE_ID = localStorage.getItem("assignedTable") || "1";
 
