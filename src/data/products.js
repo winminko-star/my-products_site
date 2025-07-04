682962e39d30ed22f12442951dc223061a60f3b9
winminko-star <winminthuzar1@gmail.com>
2025-07-04 21:20:42 +0800

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,20 +1,51 @@
 const products = [
   // Food
-  { id: 1, name: "Fried Rice", unit: "plate", price: 3000, category: "Food" },
-  { id: 2, name: "Chicken Curry", unit: "bowl", price: 3500, category: "Food" },
-  { id: 3, name: "Noodles", unit: "plate", price: 2500, category: "Food" },
+  { id: 1, name: "Fried Rice", unit: "plate", price: 3000, category: "Food", image: "/images/fried_rice.png" },
+  { id: 2, name: "Chicken Curry", unit: "bowl", price: 3500, category: "Food", image: "/images/chicken_curry.png" },
+  { id: 3, name: "Noodles", unit: "plate", price: 2500, category: "Food", image: "/images/noodles.png" },
+  { id: 4, name: "Grilled Fish", unit: "plate", price: 5000, category: "Food", image: "/images/grilled_fish.png" },
+  { id: 5, name: "Beef Steak", unit: "plate", price: 7000, category: "Food", image: "/images/beef_steak.png" },
+  { id: 6, name: "Pork Curry", unit: "bowl", price: 4000, category: "Food", image: "/images/pork_curry.png" },
+  { id: 7, name: "Tofu Salad", unit: "bowl", price: 2800, category: "Food", image: "/images/tofu_salad.png" },
+  { id: 8, name: "Mutton Biryani", unit: "plate", price: 5500, category: "Food", image: "/images/mutton_biryani.png" },
+  { id: 9, name: "Sweet and Sour Chicken", unit: "plate", price: 4200, category: "Food", image: "/images/sweet_and_sour_chicken.png" },
+  { id: 10, name: "Seafood Platter", unit: "plate", price: 8000, category: "Food", image: "/images/seafood_platter.png" },
 
   // Soup
-  { id: 4, name: "Tomato Soup", unit: "bowl", price: 2000, category: "Soup" },
-  { id: 5, name: "Corn Soup", unit: "bowl", price: 2200, category: "Soup" },
+  { id: 11, name: "Tomato Soup", unit: "bowl", price: 2000, category: "Soup", image: "/images/tomato_soup.png" },
+  { id: 12, name: "Corn Soup", unit: "bowl", price: 2200, category: "Soup", image: "/images/corn_soup.png" },
+  { id: 13, name: "Pumpkin Soup", unit: "bowl", price: 2400, category: "Soup", image: "/images/pumpkin_soup.png" },
+  { id: 14, name: "Vegetable Soup", unit: "bowl", price: 1800, category: "Soup", image: "/images/vegetable_soup.png" },
+  { id: 15, name: "Chicken Soup", unit: "bowl", price: 2500, category: "Soup", image: "/images/chicken_soup.png" },
+  { id: 16, name: "Miso Soup", unit: "bowl", price: 2100, category: "Soup", image: "/images/miso_soup.png" },
+  { id: 17, name: "Fish Soup", unit: "bowl", price: 2600, category: "Soup", image: "/images/fish_soup.png" },
+  { id: 18, name: "Beef Broth", unit: "bowl", price: 2700, category: "Soup", image: "/images/beef_broth.png" },
+  { id: 19, name: "Hot and Sour Soup", unit: "bowl", price: 2300, category: "Soup", image: "/images/hot_and_sour_soup.png" },
+  { id: 20, name: "Crab Soup", unit: "bowl", price: 3000, category: "Soup", image: "/images/crab_soup.png" },
 
   // Drink
-  { id: 6, name: "Coke", unit: "bottle", price: 1500, category: "Drink" },
-  { id: 7, name: "Sprite", unit: "bottle", price: 1500, category: "Drink" },
+  { id: 21, name: "Coke", unit: "bottle", price: 1500, category: "Drink", image: "/images/coke.png" },
+  { id: 22, name: "Sprite", unit: "bottle", price: 1500, category: "Drink", image: "/images/sprite.png" },
+  { id: 23, name: "Pepsi", unit: "bottle", price: 1500, category: "Drink", image: "/images/pepsi.png" },
+  { id: 24, name: "7-Up", unit: "bottle", price: 1500, category: "Drink", image: "/images/7up.png" },
+  { id: 25, name: "Orange Juice", unit: "glass", price: 2000, category: "Drink", image: "/images/orange_juice.png" },
+  { id: 26, name: "Apple Juice", unit: "glass", price: 2000, category: "Drink", image: "/images/apple_juice.png" },
+  { id: 27, name: "Lemonade", unit: "glass", price: 1800, category: "Drink", image: "/images/lemonade.png" },
+  { id: 28, name: "Iced Tea", unit: "glass", price: 1800, category: "Drink", image: "/images/iced_tea.png" },
+  { id: 29, name: "Mango Smoothie", unit: "glass", price: 2500, category: "Drink", image: "/images/mango_smoothie.png" },
+  { id: 30, name: "Milkshake", unit: "glass", price: 2500, category: "Drink", image: "/images/milkshake.png" },
 
   // Others
-  { id: 8, name: "Tissue", unit: "pack", price: 500, category: "Others" },
-  { id: 9, name: "Ice", unit: "cup", price: 300, category: "Others" },
+  { id: 31, name: "Tissue", unit: "pack", price: 500, category: "Others", image: "/images/tissue.png" },
+  { id: 32, name: "Ice", unit: "cup", price: 300, category: "Others", image: "/images/ice.png" },
+  { id: 33, name: "Plastic Spoon", unit: "set", price: 200, category: "Others", image: "/images/plastic_spoon.png" },
+  { id: 34, name: "Fork & Knife", unit: "set", price: 400, category: "Others", image: "/images/fork_and_knife.png" },
+  { id: 35, name: "Napkin", unit: "pack", price: 500, category: "Others", image: "/images/napkin.png" },
+  { id: 36, name: "Toothpick", unit: "pack", price: 100, category: "Others", image: "/images/toothpick.png" },
+  { id: 37, name: "Paper Cup", unit: "cup", price: 200, category: "Others", image: "/images/paper_cup.png" },
+  { id: 38, name: "Straw", unit: "piece", price: 50, category: "Others", image: "/images/straw.png" },
+  { id: 39, name: "Table Mat", unit: "piece", price: 700, category: "Others", image: "/images/table_mat.png" },
+  { id: 40, name: "Salt Packet", unit: "pack", price: 100, category: "Others", image: "/images/salt_packet.png" },
 ];
 
 export default products;
