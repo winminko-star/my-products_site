274d50007c840cd9d56904680b9ae2925b538759
winminko-star <winminthuzar1@gmail.com>
2025-07-05 23:16:12 +0800

    Update SummaryPage.jsx

--- src/pages/SummaryPage.jsx
+++ src/pages/SummaryPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getDatabase, ref, get } from "firebase/database";
-import app from "../firebase";
+import app,db from "../firebase";
 import "../index.css";
 
 export default function SummaryPage() {
