a009091ac5455eaa425b4f3b8f34d34f815327db
winminko-star <winminthuzar1@gmail.com>
2025-07-07 21:58:59 +0800

    Delete src/components/RequireAdmin.jsx

--- src/components/RequireAdmin.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/RequireAdmin.jsx
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export default function RequireAdmin({ children }) {
-  const isAllowed = localStorage.getItem("adminAccess") === "true";
-  return isAllowed ? children : <Navigate to="/admin-login" replace />;
-}
